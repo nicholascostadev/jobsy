c48517dac3c2471b2830654c40c75f5ca2d6fe3c
Nicholas Costa <nicholascostadev@gmail.com>
2023-05-19 14:55:57 -0300

    refactor: split Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<header class="shadow-sm">
+    <nav class="h-header flex justify-between items-center px-layout w-layout mx-auto max-w-full">
+        <a href="/" class="text-2xl font-semibold font-poppins">Jobsy</a>
+
+        <div class="flex items-center justify-center gap-4">
+            <a href="/#about" class="hover:text-gray-700 transition-colors">About Us</a>
+            {#if $page.data.user}
+                <a href="/feed">Jobs</a>
+                <a href="/jobs/create">Post a Job</a>
+                <form method="POST">
+                    <button formaction="/logout" class="hover:text-gray-700" type="submit">
+                        Logout
+                    </button>
+                </form>
+            {/if}
+            {#if !$page.data.user}
+                <a
+                    href="/login"
+                    class="text-lg bg-purple-500 rounded-full px-3 py-1 text-white hover:bg-purple-600 transition-colors"
+                >
+                    Login
+                </a>
+            {/if}
+            <a
+                href="/{$page.data.user?.username}"
+                class="bg-gray-400 w-10 h-10 rounded-full"
+                aria-label="Go to profile"
+            />
+        </div>
+    </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,7 @@
 <script>
+    import Header from '$lib/components/Header.svelte';
     import '../app.postcss';
-
-    export let data;
 </script>
 
-<header class="shadow-sm">
-    <nav class="h-header flex justify-between items-center px-layout w-layout mx-auto max-w-full">
-        <a href="/" class="text-2xl font-semibold font-poppins">Jobsy</a>
-
-        <div class="flex items-center justify-center gap-4">
-            <a href="/#about" class="hover:text-gray-700 transition-colors">About Us</a>
-            {#if data.user}
-                <a href="/feed">Jobs</a>
-                <a href="/jobs/create">Post a Job</a>
-                <form method="POST">
-                    <button formaction="/logout" class="hover:text-gray-700" type="submit">
-                        Logout
-                    </button>
-                </form>
-            {/if}
-            {#if !data.user}
-                <a
-                    href="/login"
-                    class="text-lg bg-purple-500 rounded-full px-3 py-1 text-white hover:bg-purple-600 transition-colors"
-                >
-                    Login
-                </a>
-            {/if}
-            <a
-                href="/{data.user?.username}"
-                class="bg-gray-400 w-10 h-10 rounded-full"
-                aria-label="Go to profile"
-            />
-        </div>
-    </nav>
-</header>
+<Header />
 <slot />
